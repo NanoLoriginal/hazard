91c49e7df0eef6a363da2fabfc0d56f184bd2948
NanoLoriginal <kyliann.faggian@gmail.com>
2022-04-04 13:30:32 +0200

    cleanup

---
+++ assets/tilemaps/platformPack_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="platformPack_tilesheet" tilewidth="32" tileheight="32" tilecount="392" columns="28">
+ <image source="../../devoirBlanc/Fourras/porte+save+move/assets/tilesets/platformPack_tilesheet.png" width="896" height="448"/>
+</tileset>
